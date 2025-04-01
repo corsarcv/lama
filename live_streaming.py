8b3601bcf603264d4553e9300508643024b6d08d
corsarcv <igor.cors@gmail.com>
2025-04-01 00:45:09 -0400

    verify predictions

--- live_streaming.py
+++ live_streaming.py
@@ -47,13 +47,16 @@ def process_stock_data(live_event: Dict[str, Any]):
     Implement your logic here (e.g., store in DB, trigger alerts, etc.).
     """
     symbol = live_event['stock']
-    logging.info(f"Received data for processing for {symbol}}")
+    logging.info(f"Received data for processing for {symbol}")
     historical_events[symbol].append(live_event)
     sector = get_sector_for_sp500_ticker(live_event['stock'])
     prediction = StockSuggester(sector=sector).predict(historical_events[symbol])
     logging.debug(prediction)
 
-
+# ========================
+# 🔹 In case if model suggest to buy/sell, 
+# add the suggestion to the list to analyze later
+# ========================
 def post_process_suggestion(prediction, symbol):
     action = prediction['action']
     if action in (Action.STRONG_BUY, Action.BUY):
@@ -108,27 +111,8 @@ async def handle_bar(bar):
             "volume": volume,
             "moving_average": moving_average # Will be None if not enough data yet
         }
-        # Add event to the queue
-        # Only add event if the queue full or empty (first request or oldest event is greater then the time interval)
-        # Check if the queue is full or if the oldest event is older than the interval
-        price_history_for_symbol = live_events_history[symbol]
 
-        if len(price_history_for_symbol) == Config.TIME_INTERVAL_MINS:
-            logging.debug(f"Queue for {symbol} is full. Cleaning the queue and processing event.")
-            live_events_history[symbol] = deque(maxlen=Config.TIME_INTERVAL_MINS)
-            should_process = True
-        elif len(price_history_for_symbol) == 0:
-            logging.debug(f"Queue for {symbol} is empty. Processing event.")
-            should_process = True            
-        else:
-            oldest_event_timestamp = pd.Timestamp(price_history_for_symbol[0]['time'])
-            current_timestamp = pd.Timestamp(timestamp_iso)
-            time_diff = current_timestamp - oldest_event_timestamp
-            if time_diff >= pd.Timedelta(minutes=Config.TIME_INTERVAL_MINS):
-                logging.debug(f"Oldest event for {symbol} is older than {Config.TIME_INTERVAL_MINS} minutes. Processing events.")
-                should_process = True
-            else:
-                should_process = False
+        should_process = should_process_event(symbol, timestamp_iso)
         live_events_history.append(live_event_data)
         
         if should_process:
@@ -137,34 +121,71 @@ async def handle_bar(bar):
         else:
             logging.debug('Skipping event processing')
 
-        # Check predictions and if we have a predition for the stock passed due (target time < now), then remove that preditction from the list
-        now = pd.Timestamp.now()
-        for symbol, pred_list in predictions.items():
-            for pred in pred_list[:]:  # Iterate over a copy to allow removal
-                if pred['target_date'] < now:
-                    logging.info(f"Prediction for {symbol} became due with a price {live_event_data['price']}: {pred}")
-                    predictions[symbol].remove(pred)
-                    
+        check_due_predictions(symbol, live_event_data)
 
     except Exception as e:
         logging.error(f"Error processing bar for {getattr(bar, 'symbol', 'N/A')}: {e}", exc_info=True)
 
 
-async def subscribe_and_process_bars(symbols: List[str]):
+# ========================
+# 🔹 Only add event if the queue full or empty (first request)
+#  or oldest event is greater then the time interval)
+# ========================   
+def should_process_event(symbol, timestamp_iso):
+    price_history_for_symbol = live_events_history[symbol]        
+    if len(price_history_for_symbol) == Config.TIME_INTERVAL_MINS:
+        logging.debug(f"Queue for {symbol} is full. Cleaning the queue and processing event.")
+        live_events_history[symbol] = deque(maxlen=Config.TIME_INTERVAL_MINS)
+        should_process = True
+    elif len(price_history_for_symbol) == 0:
+        logging.debug(f"Queue for {symbol} is empty. Processing event.")
+        should_process = True            
+    else:
+        oldest_event_timestamp = pd.Timestamp(price_history_for_symbol[0]['time'])
+        current_timestamp = pd.Timestamp(timestamp_iso)
+        time_diff = current_timestamp - oldest_event_timestamp
+        if time_diff >= pd.Timedelta(minutes=Config.TIME_INTERVAL_MINS):
+            logging.debug(f"Oldest event for {symbol} is older than {Config.TIME_INTERVAL_MINS} minutes. Processing events.")
+            should_process = True
+        else:
+            should_process = False
+    return should_process
+
+# ========================
+# 🔹 Check accuracy of old predictions becoming due
+# ========================   
+def check_due_predictions(symbol, live_event_data):
+    # Check predictions and if we have a predition for the stock passed due (target time < now), then remove that preditction from the list
+    now = pd.Timestamp.now()
+    for symbol, pred_list in predictions.items():
+        for pred in pred_list[:]:  # Iterate over a copy to allow removal
+            if pred['target_date'] < now:
+                logging.info(f"Prediction for {symbol} became due with a price {live_event_data['price']}: {pred}")
+                predictions[symbol].remove(pred)
+
+# ========================
+# 🔹 Main entry point: subscribing and registering a callback for live price events
+# ========================
+async def subscribe_and_process_bars():
     """
     Subscribes to Alpaca's real-time bar stream for the given symbols
     and processes incoming bars using the handle_bar callback.
     """
+
     if not API_KEY or not SECRET_KEY:
         logging.error("API Key ID or Secret Key not found in environment variables.")
         print("Error: Set APCA_API_KEY_ID and APCA_API_SECRET_KEY environment variables.")
         return
+    
+    # List of stocks to monitor
+    stock_symbols = load_watchlist()
+    print(f"Monitoring {len} stocks: {', '.join(stock_symbols)}")
 
-    logging.info(f'Getting recent history for stocks {symbols}')
-    for symbol in symbols:
+    logging.info(f'Getting recent history for stocks {stock_symbols}')
+    for symbol in stock_symbols:
         historical_events[symbol] = collect_events_history(symbol)
 
-    logging.info(f"Attempting to connect to Alpaca Stream API at {BASE_URL} for symbols: {symbols}")
+    logging.info(f"Attempting to connect to Alpaca Stream API at {BASE_URL} for symbols: {stock_symbols}")
     logging.info(f"Using data feed: {DATA_FEED}")
 
     # Instantiate Stream
@@ -177,7 +198,7 @@ async def subscribe_and_process_bars(symbols: List[str]):
 
     # Register the handler for bars for the specified symbols
     # The '*' unpacks the list of symbols
-    stream.subscribe_bars(handle_bar, *symbols)
+    stream.subscribe_bars(handle_bar, *stock_symbols)
 
     logging.info("Subscription successful. Starting stream listener...")
     try:
@@ -194,19 +215,16 @@ async def subscribe_and_process_bars(symbols: List[str]):
 
 
 # ========================
-# 🔹 Example Usage
+# 🔹 EStarting main app loop
 # ========================
 if __name__ == "__main__":
-    # List of stocks to monitor
-    stock_symbols = load_watchlist()
 
     print("Starting Alpaca data stream subscription...")
-    print(f"Monitoring {len} stocks: {', '.join(stock_symbols)}")
     print("Press Ctrl+C to stop.")
 
     # Run the main async function using asyncio's event loop
     try:
-        asyncio.run(subscribe_and_process_bars(stock_symbols))
+        asyncio.run(subscribe_and_process_bars())
     except KeyboardInterrupt:
         print("\nExiting application.")
     except Exception as e:
